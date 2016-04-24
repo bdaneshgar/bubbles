4ebdf3aed31b4f8e23ee10718b5068cc1f88f5ed
Brian Daneshgar <briandaneshgar@gmail.com>
2016-04-23 19:18:19 -0700

    single bubble pop

--- Bubbles/GameScene.swift
+++ Bubbles/GameScene.swift
@@ -11,33 +11,33 @@ import SpriteKit
 //hello world!
 
 class GameScene: SKScene {
+    
+    var score = 0
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 45;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
         
-        self.addChild(myLabel)
+        let sprite = SKSpriteNode(imageNamed: "Bubble")
+        sprite.name = "bubble"
+        sprite.size = CGSize(width: 180, height: 180)
+        sprite.position = CGPoint(x: self.frame.size.width/2, y: self.frame.size.height/2)
+        sprite.physicsBody = SKPhysicsBody(circleOfRadius: 95)
+        sprite.physicsBody?.dynamic = false
+        self.addChild(sprite)
+        
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
-        
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+        for touch: AnyObject in touches {
+            let location = (touch as! UITouch).locationInNode(self)
+            if let theName = self.nodeAtPoint(location).name {
+                if theName == "bubble" {
+                    self.removeChildrenInArray([self.nodeAtPoint(location)])
+                    score+=1
+                    print("score")
+                }
+            }
         }
     }
    

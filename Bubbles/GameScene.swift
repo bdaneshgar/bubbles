2dfd1a5faf8fcc3fc049ad7049484311383f9c8c
Salah Ahmed <salah.ahmed.hslc@gmail.com>
2016-04-24 23:33:40 -0400

    create bubble class, add readme

--- Bubbles/GameScene.swift
+++ Bubbles/GameScene.swift
@@ -11,21 +11,16 @@ import SpriteKit
 //hello world!
 
 class GameScene: SKScene {
-    
+
     var score = 0
-    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        
-        createBubble()
-        createBubble()
-        createBubble()
-        createBubble()
-        createBubble()
-        createBubble()
-
+        let original_bubbles = 6
+        for i in 1 ... original_bubbles {
+            self.addChild(Bubble().bubble)
+        }
     }
-    
+
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
         for touch: AnyObject in touches {
@@ -33,30 +28,29 @@ class GameScene: SKScene {
             if let theName = self.nodeAtPoint(location).name {
                 if theName == "bubble" {
                     self.removeChildrenInArray([self.nodeAtPoint(location)])
-                    score+=1
+                    score += 1
                     print(score)
-                    createBubble()
+                    self.addChild(Bubble().bubble)
                 }
             }
         }
     }
-   
+
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
-    
-    func createBubble(){
-        
-        let newX = Int(arc4random()%1024)
-        let newY = Int(arc4random()%1024)
-        
-        let sprite = SKSpriteNode(imageNamed: "Bubble")
-        sprite.name = "bubble"
-        sprite.size = CGSize(width: 180, height: 180)
-        sprite.position = CGPoint(x: newX, y: newY)
-        sprite.physicsBody = SKPhysicsBody(circleOfRadius: 95)
-        sprite.physicsBody?.dynamic = false
-        self.addChild(sprite)
-        
+}
+
+class Bubble {
+    let bubble: SKSpriteNode
+    init(){
+        let newX = Int(arc4random_uniform(1024))
+        let newY = Int(arc4random_uniform(1024))
+        self.bubble = SKSpriteNode(imageNamed: "Bubble")
+        self.bubble.name = "bubble"
+        self.bubble.size = CGSize(width: 180, height: 180)
+        self.bubble.position = CGPoint(x: newX, y: newY)
+        self.bubble.physicsBody = SKPhysicsBody(circleOfRadius: 95)
+        self.bubble.physicsBody?.dynamic = false
     }
 }
---
+++ README.md
@@ -0,0 +1,5 @@
+# Bubbles :)
+
+A simple application for children or the bored 
+
+pop bubbles by tapping the screen

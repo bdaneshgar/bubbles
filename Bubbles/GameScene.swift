4a417aec8c54ba68aa74f051434ad8ca14eefc60
Salah Ahmed <salah.ahmed.hslc@gmail.com>
2016-04-25 16:14:16 -0400

    use foreach, combine if paths

--- Bubbles/GameScene.swift
+++ Bubbles/GameScene.swift
@@ -5,19 +5,17 @@
 //  Created by Brian Daneshgar on 4/23/16.
 //  Copyright (c) 2016 Brian Daneshgar. All rights reserved.
 //
-
 import SpriteKit
 
-//hello world!
 
 class GameScene: SKScene {
-
     var score = 0
+    var bubbleName = "bubble"
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         let original_bubbles = 6
-        for i in 1 ... original_bubbles {
-            self.addChild(Bubble().bubble)
+        (1...original_bubbles).forEach { (i) in
+            self.addChild(Bubble(name: bubbleName).bubble)
         }
     }
 
@@ -25,13 +23,12 @@ class GameScene: SKScene {
        /* Called when a touch begins */
         for touch: AnyObject in touches {
             let location = (touch as! UITouch).locationInNode(self)
-            if let theName = self.nodeAtPoint(location).name {
-                if theName == "bubble" {
+            let name = self.nodeAtPoint(location).name
+            if  name != nil && name! == bubbleName {
                     self.removeChildrenInArray([self.nodeAtPoint(location)])
                     score += 1
                     print(score)
-                    self.addChild(Bubble().bubble)
-                }
+                    self.addChild(Bubble(name: bubbleName).bubble)
             }
         }
     }
@@ -41,16 +38,20 @@ class GameScene: SKScene {
     }
 }
 
+
 class Bubble {
     let bubble: SKSpriteNode
-    init(){
-        let newX = Int(arc4random_uniform(1024))
-        let newY = Int(arc4random_uniform(1024))
+    let random_limit: UInt32 = 1024
+    let diameter = 180
+    let radius: CGFloat = 95
+    init(name: String){
+        let xpos = Int(arc4random_uniform(random_limit))
+        let ypos = Int(arc4random_uniform(random_limit))
         self.bubble = SKSpriteNode(imageNamed: "Bubble")
-        self.bubble.name = "bubble"
-        self.bubble.size = CGSize(width: 180, height: 180)
-        self.bubble.position = CGPoint(x: newX, y: newY)
-        self.bubble.physicsBody = SKPhysicsBody(circleOfRadius: 95)
+        self.bubble.name = name
+        self.bubble.size = CGSize(width: diameter, height: diameter)
+        self.bubble.position = CGPoint(x: xpos, y: ypos)
+        self.bubble.physicsBody = SKPhysicsBody(circleOfRadius: radius)
         self.bubble.physicsBody?.dynamic = false
     }
 }
